835cb371330585f2c6bca00682bce435ac6d7594
Davinder9297 <davindergiri765@gmail.com>
2024-03-14 13:46:18 +0530

    first commit

---
+++ src/Components/MyLearning/MyLearning.js
@@ -0,0 +1,11 @@
+import Header from './Header';
+import Mycourse from './Mycourse';
+
+export default function MyLearing(){
+    return (
+        <div>
+            <Header/>
+            <Mycourse/>
+        </div>
+    );
+}
---
+++ src/Routing/route.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from '../Components/Header'
+import Herosection from '../Components/Herosection'
+import Companies from '../Components/Companies'
+import Courses_Home from '../Components/Courses_Home'
+import DetailCourses from '../Components/DetailCourses'
+import Home from '../Components/Home'
+
+
+
+const Router = () => {
+    return (
+     
+            <BrowserRouter >
+                <Navbar/>
+
+                <Routes>
+                   <Route path='/' element={<Home/>}/>
+                   <Route path='/a' element={<DetailCourses/>}/>
+                </Routes>
+            </BrowserRouter >
+      
+    )
+}
+
+export default Router
